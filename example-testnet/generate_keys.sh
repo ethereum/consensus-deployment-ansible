fa91d76146500eb9e0da0a4a5ccdebb95f0acdef
parithosh <parithosh.jayanthi@ethereum.org>
2021-10-07 19:09:09 +0300

    push everything

--- example-testnet/generate_keys.sh
+++ example-testnet/generate_keys.sh
@@ -1,6 +1,7 @@
 validators_per_host=100 # Number of validators in each host
 
-source vars.env
+export VALIDATORS_MNEMONIC_0="flock beyond move size arena limit shoe observe ship drop bacon phone satisfy lend evolve anchor sheriff when impulse physical certain future trophy series"
+
 if [ -z "$VALIDATORS_MNEMONIC_0" ]; then
   echo "missing mnemonic 0"
   exit 1
@@ -12,7 +13,6 @@ function prep_group {
   let offset=$3
   let keys_to_create=$4
   naming_prefix="$5"
-
   echo "Group base: $group_base"
   for (( i = 0; i < keys_to_create; i++ )); do
     let node_index=group_base+i
@@ -20,8 +20,10 @@ function prep_group {
     let validators_source_min=offset_i*validators_per_host
     let validators_source_max=validators_source_min+validators_per_host
 
+    echo $validators_source_mnemonic
     echo "writing keystores for host $naming_prefix-$node_index: $validators_source_min - $validators_source_max"
     eth2-val-tools keystores \
+    --insecure
     --prysm-pass="prysm" \
     --out-loc="validator_prep/$naming_prefix-$node_index" \
     --source-max="$validators_source_max" \
@@ -39,5 +41,5 @@ prep_group 1 "$VALIDATORS_MNEMONIC_0" 1 1 "alpha-lodestar"
 echo "Nimbus keys"
 prep_group 1 "$VALIDATORS_MNEMONIC_0" 2 1 "alpha-nimbus"
 
-#echo "Teku keys"
-#prep_group 1 "$VALIDATORS_MNEMONIC_0" 3 4 "alpha-teku"
+echo "Teku keys"
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 3 1 "alpha-teku"
